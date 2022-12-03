3d29854b04870130a7a10242e6cd5df952403af0
makcol <118094961+makcol@users.noreply.github.com>
2022-12-03 12:46:49 +0800

    Update baidu.py

--- baidu.py
+++ baidu.py
@@ -11,45 +11,53 @@ headers = {
     'Host': 'top.baidu.com',
     'Upgrade-Insecure-Requests': '1',
 }
+
 # 获取百度热榜
 bd_hot_url = 'https://top.baidu.com/board?tab=novel'
 response = requests.get(bd_hot_url, headers=headers)
 html = etree.HTML(response.text)
-bd_hot_block = html.xpath("//div[contains(@class,'category-wrap_iQLoo')]")
-book_list = []
-for single in bd_hot_block:
-    book = {}
-    # 获取标题
-    title = single.xpath('string(.//div[@class="c-single-text-ellipsis"]/text())')
-    print('标题是：{}'.format(title))
-    book['title'] = title
-    # 小说获取作者
-    author = single.xpath('string(.//div[@class="intro_1l0wp"]/text())')
-    print('作者是：{}'.format(author))
-    book['author'] = author
-    # # 获取链接
-    # url = single.xpath('string(./a/@href)')
-    # print('链接是：{}'.format(url))
-    # 获取热搜排名
-    rank = single.xpath('string(./a/div/text())')
-    print('排名是：{}'.format(rank))
-    book['rank'] = rank
-    # 获取图片地址
-    pic = single.xpath('string(./a/img/@src)')
-    print('图片地址是：{}'.format(pic))
-    book['pic'] = pic
-    # 获取热搜指数hot-index
-    zhishu = single.xpath('string(.//div[contains(@class,"hot-index_1Bl1a")]/text())')
-    print('热搜指数是：{}'.format(zhishu))
-    book['zhishu'] = zhishu
-    # 获取简介hot-desc
-    desc = single.xpath('string(.//div[contains(@class,"desc_")]/text())')
-    print('描述是：{}'.format(desc))
-    book['desc'] = desc
-    book_list.append(book)
+type_block = html.xpath("//div[contains(@class,'tag-item_2erEC')]/text()")
+print(type_block)
+book_map = {}
+for item in type_block:
+    hot_url = "https://top.baidu.com/board?platform=pc&tab=novel&tag={\"category\":\"" + item.strip() + "\"}"
+    response = requests.get(hot_url, headers=headers)
+    html = etree.HTML(response.text)
+    bd_hot_block = html.xpath("//div[contains(@class,'category-wrap_iQLoo')]")
+    book_list = []
+    for single in bd_hot_block:
+        book = {}
+        # 获取标题
+        title = single.xpath('string(.//div[@class="c-single-text-ellipsis"]/text())')
+        print('标题是：{}'.format(title))
+        book['title'] = title
+        # 小说获取作者
+        author = single.xpath('string(.//div[@class="intro_1l0wp"]/text())')
+        print('作者是：{}'.format(author))
+        book['author'] = author
+        # # 获取链接
+        # url = single.xpath('string(./a/@href)')
+        # print('链接是：{}'.format(url))
+        # 获取热搜排名
+        rank = single.xpath('string(./a/div/text())')
+        print('排名是：{}'.format(rank))
+        book['rank'] = rank
+        # 获取图片地址
+        pic = single.xpath('string(./a/img/@src)')
+        print('图片地址是：{}'.format(pic))
+        book['pic'] = pic
+        # 获取热搜指数hot-index
+        zhishu = single.xpath('string(.//div[contains(@class,"hot-index_1Bl1a")]/text())')
+        print('热搜指数是：{}'.format(zhishu))
+        book['zhishu'] = zhishu
+        # 获取简介hot-desc
+        desc = single.xpath('string(.//div[contains(@class,"desc_")]/text())')
+        print('描述是：{}'.format(desc))
+        book['desc'] = desc
+        book_list.append(book)
+    book_map[item.strip()] = book_list
 
-output_json = json.dumps(book_list)
-print(output_json)
-file = open("book.json","w")
-file.write(output_json)
+output_json = json.dumps(book_map,ensure_ascii=False)
+file = open("book.json", "w", encoding='utf-8')
+file.write(output_json.replace(u'\xa0', u' '))
 file.close()
